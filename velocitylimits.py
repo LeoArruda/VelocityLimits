20c82af37944360ec22523712e8bd65066c1bbaf
Leo Arruda <lvarruda@gmail.com>
2021-01-27 18:32:02 -0500

    feat: add Main method

---
+++ velocitylimits.py
@@ -0,0 +1,8 @@
+from model.client import Client
+from io.load import load
+from config.config import MAXDAILYLOAD, MAXDAILYTRANSACTIONS, MAXWEEKLYLOAD, INPUTFILE, OUTPUTFILE
+
+
+if __name__ == '__main__':
+    print(INPUTFILE)
+    print(OUTPUTFILE)
