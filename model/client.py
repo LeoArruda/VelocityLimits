6756525b32362f5d5b62a0aed172fdafbb60217d
Leo Arruda <lvarruda@gmail.com>
2021-01-26 22:50:06 -0500

    feat: add Client model

--- model/client.py
+++ model/client.py
@@ -7,8 +7,8 @@ class Client():
     Creates a Client object to handle all features of an account.
     Usage: myclient = Client(client_ID)
     """
-    def __init__(self, client_ID):
-        self.client_id = client_ID
+    def __init__(self, client_id):
+        self.client_id = client_id
         self.daily_accum_loads = 0
         self.currentday_load_ammount = 0
         self.currentweek_load_ammount = 0
@@ -49,11 +49,18 @@ class Client():
         """
         Add initiate a new day in case the date changed
         """
+        # Todo:
+        # - Evaluate if the load date is a new day and initiate the day
+        # - Evaluate if the day is starting a new week and initiate the week
 
     def load_transaction(self, load_amount, load_date):
         """
         Evaluates the business rules and executes the new load case possible
         """
+        # Todo: 
+        # - Initiate a new day case possible
+        # - Evaluate Business rules
+        # - update Client loads
     
 
 
