7a467be4fdbd49089e6e8875c720ef7eaa4524d9
Leo Arruda <lvarruda@gmail.com>
2021-01-27 18:26:12 -0500

    feat: add Load method

---
+++ io/load.py
@@ -0,0 +1,13 @@
+from config.config import OUTPUTFILE
+import json
+from model.client import Client
+
+def load(filename):
+    """
+    """
+    # Todo:
+    # - Open the filename
+    # - Store the clients
+    # - Store the loaded transactions
+    # - Write the expected output
+
